854ac8e64cc5580ac517d4950bfc165a71401920
Fathimathul Mizba <mizba@kiebot.com>
2026-03-13 13:15:57 +0530

    use DATABASE_URL for neon

--- config/database.js
+++ config/database.js
@@ -1,16 +1,15 @@
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
-);
+});
+
+module.exports = sequelize;
